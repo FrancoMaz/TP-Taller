1e43a137ce8b49fae1edf9d98052c502d324d8a9
Pabloino <pablo-ino@hotmail.com>
2016-08-30 02:19:57 -0300

    Vuelvo a agregar Servidor.h por un error

---
+++ TP1/Servidor.h
@@ -0,0 +1,38 @@
+/*
+ * Servidor.h
+ *
+ *  Created on: 28 ago. 2016
+ *      Author: franco
+ */
+
+#ifndef TP1_SERVIDOR_H_
+#define TP1_SERVIDOR_H_
+
+#include <string>
+#include <list>
+#include <queue>
+using namespace std;
+
+class Servidor {
+public:
+	Servidor(string nombreArchivoDeUsuarios, int puerto);
+	virtual ~Servidor();
+	list<Cliente> autenticar(string nombre, string contrasenia);
+	list<Cliente> obtenerClientes();
+	void guardarLog();
+	list<string> obtenerMensajes(Cliente cliente);
+	void crearMensaje(Cliente clienteOrigen, Cliente clienteDestinatario);
+
+private:
+	int puerto;
+	int socket;
+	ArchivoUsuario archivoUsuarios;
+	queue<string> colaMensajesNoProcesados;
+	TablaDeHash hashMensajesProcesados;
+	Log archivoLog;
+	pthread_t threadProcesador;
+	pthread_t threadChecker;
+	TablaDeHash hashThreadsClientes;
+};
+
+#endif /* TP1_SERVIDOR_H_ */
