cd6fe3c9a4a350a1400b6ecfaaa6e4477209d2c3
josefinamont <mariajosefina.mont@gmail.com>
2016-09-04 21:52:55 -0300

    Se inicializa el socket del servidor para comenzar a escuchar, con la ip y puerto correspondientes

--- TP1/Servidor.cpp
+++ TP1/Servidor.cpp
@@ -6,11 +6,29 @@
  */
 
 #include "Servidor.h"
+#include <string.h>
 using namespace std;
 
 Servidor::Servidor(char* nombreArchivoDeUsuarios, int puerto) {
 	this->puerto = puerto;
 	this->nombreArchivo = nombreArchivoDeUsuarios;
+	this->welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+	/*---- Configure settings of the server address struct ----*/
+	/* Address family = Internet */
+	this->serverAddr.sin_family = AF_INET;
+	/* Set port number, using htons function to use proper byte order */
+	this->serverAddr.sin_port = htons(puerto);
+	/* Set IP address to localhost */
+	this->serverAddr.sin_addr.s_addr = inet_addr("192.168.1.10");
+	/* Set all bits of the padding field to 0 */
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+	/*---- Bind the address struct to the socket ----*/
+	bind(this->welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	/*---- Listen on the socket, with 5 max connection requests queued ----*/
+	if (listen(this->welcomeSocket, 6) == 0)
+		printf("Listening\n");
+	else
+		printf("Error\n");
 }
 
 Servidor::~Servidor() {
@@ -29,35 +47,37 @@ list<string>* Servidor::autenticar(string nombre, string contrasenia) {
 
 	if (myfile.is_open()) {
 
-			while (getline(myfile,linea)) {
-				nroItem = 0;
-				istringstream lineaActual(linea);
+		while (getline(myfile, linea)) {
+			nroItem = 0;
+			istringstream lineaActual(linea);
 
-				while(getline(lineaActual,csvItem,',')) {
+			while (getline(lineaActual, csvItem, ',')) {
 
-					if (nroItem == 0) {
-						nombreCapturado = csvItem;
-						usuarios.push_back(csvItem);
-					}
-					if (nroItem == 1) contraseniaCapturada = csvItem;
-					nroItem++;
+				if (nroItem == 0) {
+					nombreCapturado = csvItem;
+					usuarios.push_back(csvItem);
 				}
-				if ((nombre == nombreCapturado) && (contrasenia == contraseniaCapturada)) {
-					usuarios.remove(nombre);
-					autenticacionOK = true;
-				}
+				if (nroItem == 1)
+					contraseniaCapturada = csvItem;
+				nroItem++;
 			}
+			if ((nombre == nombreCapturado)
+					&& (contrasenia == contraseniaCapturada)) {
+				usuarios.remove(nombre);
+				autenticacionOK = true;
+			}
+		}
 	}
 	myfile.close();
 
 	if (autenticacionOK) {
 		cout << "Autenticación OK wachin" << endl;
-		for(std::list<std::string>::const_iterator i = usuarios.begin(); i != usuarios.end(); ++i) {
-		     printf("%s\n", i->c_str());
+		for (list<string>::const_iterator i = usuarios.begin();
+				i != usuarios.end(); ++i) {
+			printf("%s\n", i->c_str());
 		}
 		return users;
-	}
-	else {
+	} else {
 		cout << "Error de autenticación, no nos hackees wachin" << endl;
 		return new list<string>();
 	}
@@ -77,17 +97,17 @@ list<Mensaje> Servidor::obtenerMensajes(Cliente cliente) {
 }
 
 void Servidor::crearMensaje(Mensaje mensaje) {
-	this -> colaMensajesNoProcesados.push(mensaje);
+	this->colaMensajesNoProcesados.push(mensaje);
 }
 
-void Servidor::comenzarEscucha(){
+void Servidor::comenzarEscucha() {
 	//Metodo que pone al servidor a escuchar si alguien requiere algo.
 }
 
-void Servidor::finalizarEscucha(){
+void Servidor::finalizarEscucha() {
 	//Metodo que finaliza la escucha del servidor.
 }
 
-queue<Mensaje> Servidor::getColaMensajesNoProcesados(){
-	return this -> colaMensajesNoProcesados;
+queue<Mensaje> Servidor::getColaMensajesNoProcesados() {
+	return this->colaMensajesNoProcesados;
 }
--- TP1/Servidor.h
+++ TP1/Servidor.h
@@ -17,6 +17,8 @@
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "Cliente.h"
 #include "Mensaje.h"
 #include "Log.h"
@@ -26,11 +28,14 @@ class Servidor {
 
 private:
 	int puerto;
-	int socket;
+	int welcomeSocket;
 	char* nombreArchivo;
 	queue<Mensaje> colaMensajesNoProcesados;
 	list<Mensaje> listaMensajesProcesados;
 	Log archivoLog;
+	struct sockaddr_in serverAddr;
+	struct sockaddr_storage serverStorage;
+	socklen_t addr_size;
 	pthread_t threadProcesador;
 	pthread_t threadChecker;
 
