3d63be683244124ab96dfbee91a06e7e2b987001
raknarok8 <franco_094@hotmail.com>
2016-09-06 02:36:29 -0300

    cambios al main cliente para que no haya codigo repetido y los loops funcionen como se deben

--- TP1/MainCliente.cpp
+++ TP1/MainCliente.cpp
@@ -27,16 +27,25 @@ bool chequearSocket(string ip, int puerto) {
 }
 
 void* cicloConexion(void* arg) {
-	//Funcion que cicla para las opciones del cliente dentro del thread de comunicacion. Devuelve 0 si la opcion es desconectar, 1 si es salir.
-	Cliente cliente = *(Cliente*) arg;
-	while (cliente.getOpcionMenu() != 5 and cliente.getOpcionMenu() != 4) //mientras la opcion del menu no sea salir o desconectar..
+	//Funcion que cicla para las opciones del cliente dentro del thread de comunicacion. Devuelve 1 si la opcion es desconectar, 0 si es salir.
+	Cliente* cliente = (Cliente*) arg;
+	string user, pass;
+	while (cliente->getClientesDisponibles().empty())
 	{
-		cliente.mostrarMenuYProcesarOpcion();
+		cout << "Ingrese el nombre del usuario: ";
+		cin >> user;
+		cout << "Ingrese la password: ";
+		cin >> pass;
+		cliente->conectar(user, pass);
 	}
-	if (cliente.getOpcionMenu() == 4) {
-		return (void*) 0;
+	while (cliente->getOpcionMenu() != 5 and cliente->getOpcionMenu() != 4) //mientras la opcion del menu no sea salir o desconectar..
+	{
+		cliente->mostrarMenuYProcesarOpcion();
 	}
-	return (void*) 1;
+	if (cliente->getOpcionMenu() == 4) {
+		return (void*) 1;
+	}
+	return (void*) 0;
 }
 
 int main() {
@@ -53,66 +62,44 @@ int main() {
 		socketOk = chequearSocket(ip, puerto); //FALTA IMPLEMENTAR METODO DE CHEQUEAR IP/PUERTO. ESTA MAS ABAJO LA FUNCION.
 		if (!socketOk) {
 			//SI NO SE PUDO COMPROBAR LA CONEXION, DOY ERROR Y VUELVO A PEDIR IP Y CONEXION
-			cout
-					<< "Error: La direccion de ip o el puerto no permiten esta conexion."
-					<< endl;
+			cout << "Error: La direccion de ip o el puerto no permiten esta conexion." << endl;
 		}
 	}
 	cout << "SOCKET OK" << endl;
 	Cliente* cliente = new Cliente(ip, puerto);
 	cout << "SISTEMA DE MENSAJERIA" << endl;
 
-	do {
-		cout << "1) Conectar" << endl;
-		cout << "2) Salir" << endl;
-		cout << "Que desea hacer? " << endl;
-		cin >> accion;
-	} while (accion < 1 or accion > 2);
-
-	if (accion == 2) {
-		//si la accion es salir, devuelvo 0 y salgo del programa.
-		cout << "Saliendo del programa.. Gracias, vuelva prontoss!" << endl;
-		return 0;
-	} else {
-		string user, pass;
-		cout << "Ingrese el nombre del usuario: ";
-		cin >> user;
-		cout << "Ingrese la password: ";
-		cin >> pass;
-		cliente->conectar(user, pass);
-	}
-
-	//si no es salir, intento conectar pidiendo usuario y contraseña.
-	/*
-	 * while (cliente->getClientesDisponibles().empty())
-	 {
-	 cout << "Ingrese nombre de usuario: ";
-	 cin >> nombre;
-	 cout << "Ingrese contrasenia: ";
-	 cin >> contrasenia;
-	 cliente->setClientesDisponibles(nombre,contrasenia);
-	 if (cliente->getClientesDisponibles().empty())
-	 {
-	 //si despues de intentar conectar, sigue siendo una lista vacia, muestro error y pido nuevamente.
-	 cout << "Error al intentar autenticar. Ingrese un nombre de usuario y una contrasenia validos." << endl;
-	 }
-	 }
-	 */
-	/*int threadOk = pthread_create(&thrComu, NULL, &cicloConexion, cliente);
-	if (threadOk != 0) {
-		cout << "Error al inicializar la conexion." << endl;
-	} else {
-		cliente->setThreadComunicacion(thrComu);
-		void** resultado;
-		pthread_join(cliente->getThreadComunicacion(), (void**) &resultado);
-		int salir = *((int*) (&resultado));
-		if (salir == 0) //si es 0, es desconectar
+	do
+	{
+		do {
+			cout << "1) Conectar" << endl;
+			cout << "2) Salir" << endl;
+			cout << "Que desea hacer? " << endl;
+			cin >> accion;
+		} while (accion < 1 or accion > 2);
+		if (accion != 2) {
+			//si no es salir, creo el thread de comunicacion que intenta conectar.
+			int threadOk = pthread_create(&thrComu, NULL, &cicloConexion, cliente);
+			if (threadOk != 0) {
+				cout << "Error al inicializar la conexion." << endl;
+			}
+			else
+			{
+				cliente->setThreadComunicacion(thrComu);
+				void** resultado;
+				pthread_join(cliente->getThreadComunicacion(), (void**) &resultado); //espero que termine el thread de comunicacion que fue invocado..
+				accion = *((int*) (&resultado));
+				if (accion == 1) //si es 1, es desconectar y vuelve a ingresar al loop que ofrece conectar y desconectar
 				{
-			cout << "Desconectado del servidor.." << endl;
-		} else {
-			cout << "Saliendo del programa.. Gracias, vuelva prontoss!" << endl;
-			return 0;
+					cout << "Desconectado del servidor.." << endl;
+				}//si es 0, va a salir automaticamente del loop y del programa.
+			}
 		}
-	}*/
+		else{
+			accion = 0; //si la accion es 2, la pongo en 0 para que salga del while
+		}
+	}while (accion != 0); //si la accion es 0, es salir.
+
+	cout << "Saliendo del programa.. Gracias, vuelva prontoss!" << endl;
 	return 0;
 }
