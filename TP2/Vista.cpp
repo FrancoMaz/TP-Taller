6ab14f45b99f9082687d189495a029e98d34cc91
Pabloino <pablo-ino@hotmail.com>
2016-10-06 00:48:04 -0300

    Defino constantes a cada posición del vector textura en Vista.cpp para evitar nombrar cada textura por su número de indice y facilitar su identificación

--- TP2/Vista.cpp
+++ TP2/Vista.cpp
@@ -34,9 +34,9 @@ bool Vista::inicializar(){
 }
 
 void Vista::cargarArchivos(){
-	textura.push_back(ventana->crearTextura("Recursos/Menu_fondo.png",0));				//0: texturaMenuFondo
-	textura.push_back(ventana->crearTextura("Recursos/Menu_Metal_Slug.png",0));			//1: texturaMenuMetalSlug
-	textura.push_back(ventana->crearTextura("Recursos/Menu_Presionar_Enter.png",0));	//2: texturaPresionarEnter
+	textura.push_back(ventana->crearTextura("Recursos/Menu_fondo.png",0));
+	textura.push_back(ventana->crearTextura("Recursos/Menu_Metal_Slug.png",0));
+	textura.push_back(ventana->crearTextura("Recursos/Menu_Presionar_Enter.png",0));
 	textura.push_back(ventana->crearTextura("Recursos/Efecto_luz.png",0));				//3: texturaEfectoLuz
 	textura.push_back(ventana->crearBoton("Recursos/Boton_Salir.png"));					//4: texturaBotonSalir
 	textura.push_back(ventana->crearBoton("Recursos/Boton_Creditos.png"));				//5: texturaBotonCreditos
@@ -50,13 +50,31 @@ void Vista::cargarArchivos(){
 	textura.push_back(ventana->crearTexto("Recursos/arial.ttf",19));					//13: entradaTextoIP
 	textura.push_back(ventana->crearTexto("Recursos/msserif_bold.ttf",13));				//14: textoDatosNoCoinciden
 	textura.push_back(ventana->crearBoton("Recursos/Boton_Conectar.png"));				//15: texturaBotonConectar
+
+	//Defino constantes para cada textura (para evitar llamarlos por índices)
+	#define texturaMenuFondo textura[0]
+	#define texturaMenuMetalSlug textura[1]
+	#define texturaPresionarEnter textura[2]
+	#define texturaEfectoLuz textura[3]
+	#define texturaBotonSalir textura[4]
+	#define texturaBotonCreditos textura[5]
+	#define texturaBotonIngresar textura[6]
+	#define campoTextoUno textura[7]
+	#define campoTextoDos textura[8]
+	#define textoIngresePuerto textura[9]
+	#define textoIngreseIP textura[10]
+	#define textoTPTaller textura[11]
+	#define entradaTextoPuerto textura[12]
+	#define entradaTextoIP textura[13]
+	#define textoDatosNoCoinciden textura[14]
+	#define texturaBotonConectar textura[15]
 }
 
 void Vista::cargarPrimeraPantalla(){
 	for (float y=-290; y<40; y=y+5){
 		ventana->limpiar();
-		textura[0]->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
-		textura[1]->aplicarPosicion(180,y,0,SDL_FLIP_NONE);
+		texturaMenuFondo->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
+		texturaMenuMetalSlug->aplicarPosicion(180,y,0,SDL_FLIP_NONE);
 		ventana->actualizar();
 	}
 
@@ -65,13 +83,13 @@ void Vista::cargarPrimeraPantalla(){
 
 	for (this->opacidad; this->opacidad>0; this->opacidad=this->opacidad-3){
 		ventana->limpiar();
-		textura[0]->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
-		textura[1]->aplicarPosicion(180,40,0,SDL_FLIP_NONE);
-		textura[2]->aplicarPosicion(200,430,0,SDL_FLIP_NONE);
-		textura[11]->aplicarPosicion(254,560,0,SDL_FLIP_NONE);
-		textura[11]->setAlpha(200);
-		textura[3]->aplicarPosicionConTamanio(0,0,800,600);
-		textura[3]->setAlpha(this->opacidad);
+		texturaMenuFondo->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
+		texturaMenuMetalSlug->aplicarPosicion(180,40,0,SDL_FLIP_NONE);
+		texturaPresionarEnter->aplicarPosicion(200,430,0,SDL_FLIP_NONE);
+		textoTPTaller->aplicarPosicion(254,560,0,SDL_FLIP_NONE);
+		textoTPTaller->setAlpha(200);
+		texturaEfectoLuz->aplicarPosicionConTamanio(0,0,800,600);
+		texturaEfectoLuz->setAlpha(this->opacidad);
 		ventana->actualizar();
 	}
 
@@ -80,9 +98,9 @@ void Vista::cargarPrimeraPantalla(){
 	//Mientras la ventana no se cierre pulsando X o no se presione el enter, hacer el loop
 	while ((!controlador->comprobarCierreVentana())&&(!controlador->presionarBoton(SDLK_RETURN))){
 		ventana->limpiar();
-		textura[0]->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
-		textura[1]->aplicarPosicion(180,40,0,SDL_FLIP_NONE);
-		textura[2]->aplicarPosicion(200,430,0,SDL_FLIP_NONE);
+		texturaMenuFondo->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
+		texturaMenuMetalSlug->aplicarPosicion(180,40,0,SDL_FLIP_NONE);
+		texturaPresionarEnter->aplicarPosicion(200,430,0,SDL_FLIP_NONE);
 		if (this->opacidad>=255)	incrementa = false;
 		if (this->opacidad<=0) incrementa = true;
 		if (incrementa){
@@ -90,9 +108,9 @@ void Vista::cargarPrimeraPantalla(){
 		} else {
 			this->opacidad=this->opacidad-3;
 		}
-		textura[2]->setAlpha(this->opacidad);
-		textura[11]->aplicarPosicion(254,560,0,SDL_FLIP_NONE);
-		textura[11]->setAlpha(200);
+		texturaPresionarEnter->setAlpha(this->opacidad);
+		textoTPTaller->aplicarPosicion(254,560,0,SDL_FLIP_NONE);
+		textoTPTaller->setAlpha(200);
 		ventana->actualizar();
 	}
 	this->opacidad = 230;
@@ -124,16 +142,16 @@ datosConexion Vista::cargarPantallaIngresoDatos(bool aviso, int numeroPantalla){
 		case 2:
 			campoUno = this->datos.puerto;
 			campoDos = this->datos.ip;
-			textura[9]->actualizarTexto("Ingrese el puerto:",colorTexto);
-			textura[10]->actualizarTexto("Ingrese la IP del servidor:",colorTexto);
-			textura[14]->actualizarTexto("La dirección de ip o el puerto no permiten esta conexión",colorTextoAmarillo);
+			textoIngresePuerto->actualizarTexto("Ingrese el puerto:",colorTexto);
+			textoIngreseIP->actualizarTexto("Ingrese la IP del servidor:",colorTexto);
+			textoDatosNoCoinciden->actualizarTexto("La dirección de ip o el puerto no permiten esta conexión",colorTextoAmarillo);
 			break;
 		case 3:
 			campoUno = this->datos.nombre;
 			campoDos = this->datos.contrasenia;
-			textura[9]->actualizarTexto("Ingrese el nombre:",colorTexto);
-			textura[10]->actualizarTexto("Ingrese la contraseña:",colorTexto);
-			textura[14]->actualizarTexto("Usuario/contraseña incorrectos, inténtelo de nuevo",colorTextoAmarillo);
+			textoIngresePuerto->actualizarTexto("Ingrese el nombre:",colorTexto);
+			textoIngreseIP->actualizarTexto("Ingrese la contraseña:",colorTexto);
+			textoDatosNoCoinciden->actualizarTexto("Usuario/contraseña incorrectos, inténtelo de nuevo",colorTextoAmarillo);
 			break;
 	}
 
@@ -143,13 +161,13 @@ datosConexion Vista::cargarPantallaIngresoDatos(bool aviso, int numeroPantalla){
 	while ((!this->controlador->comprobarCierreVentana())&&(!siguiente)){
 		e = this->controlador->obtenerEvento();
 		ventana->limpiar();
-		textura[0]->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
-		textura[1]->aplicarPosicion(180,40,0,SDL_FLIP_NONE);
+		texturaMenuFondo->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
+		texturaMenuMetalSlug->aplicarPosicion(180,40,0,SDL_FLIP_NONE);
 
-		textura[9]->aplicarPosicion(312,337,0,SDL_FLIP_NONE);
-		textura[10]->aplicarPosicion(312,402,0,SDL_FLIP_NONE);
+		textoIngresePuerto->aplicarPosicion(312,337,0,SDL_FLIP_NONE);
+		textoIngreseIP->aplicarPosicion(312,402,0,SDL_FLIP_NONE);
 
-		if(textura[7]->aplicarPosicionDeBoton(310,358,&e)){
+		if(campoTextoUno->aplicarPosicionDeBoton(310,358,&e)){
 			campoUnoSeleccionado = true;
 			campoDosSeleccionado = false;
 			aviso = false;
@@ -157,11 +175,11 @@ datosConexion Vista::cargarPantallaIngresoDatos(bool aviso, int numeroPantalla){
 
 		if (campoUnoSeleccionado){
 			this->controlador->ingresarCaracteresATexto(&campoUno);
-			textura[7]->aplicarPosicionDeFrame(310,358,2,0,SDL_FLIP_NONE);
-			textura[12]->actualizarTexto(campoUno,colorTextoEntrada);
+			campoTextoUno->aplicarPosicionDeFrame(310,358,2,0,SDL_FLIP_NONE);
+			entradaTextoPuerto->actualizarTexto(campoUno,colorTextoEntrada);
 		}
 
-		if(textura[8]->aplicarPosicionDeBoton(310,423,&e)){
+		if(campoTextoDos->aplicarPosicionDeBoton(310,423,&e)){
 			campoUnoSeleccionado = false;
 			campoDosSeleccionado = true;
 			aviso = false;
@@ -169,22 +187,22 @@ datosConexion Vista::cargarPantallaIngresoDatos(bool aviso, int numeroPantalla){
 
 		if (campoDosSeleccionado){
 			this->controlador->ingresarCaracteresATexto(&campoDos);
-			textura[8]->aplicarPosicionDeFrame(310,423,2,0,SDL_FLIP_NONE);
-			textura[13]->actualizarTexto(campoDos,colorTextoEntrada);
+			campoTextoDos->aplicarPosicionDeFrame(310,423,2,0,SDL_FLIP_NONE);
+			entradaTextoIP->actualizarTexto(campoDos,colorTextoEntrada);
 		}
 
-		textura[12]->aplicarPosicion(316,363,0,SDL_FLIP_NONE);
-		textura[13]->aplicarPosicion(316,428,0,SDL_FLIP_NONE);
+		entradaTextoPuerto->aplicarPosicion(316,363,0,SDL_FLIP_NONE);
+		entradaTextoIP->aplicarPosicion(316,428,0,SDL_FLIP_NONE);
 
-		if (textura[4]->aplicarPosicionDeBoton(656,550,&e)){
+		if (texturaBotonSalir->aplicarPosicionDeBoton(656,550,&e)){
 			this->controlador->setCerrarVentana();
 		}
 
-		textura[5]->aplicarPosicionDeBoton(30,550,&e);
+		texturaBotonCreditos->aplicarPosicionDeBoton(30,550,&e);
 
 		switch(numeroPantalla){
 			case 2:
-				if(textura[6]->aplicarPosicionDeBoton(338,475,&e)){
+				if(texturaBotonIngresar->aplicarPosicionDeBoton(338,475,&e)){
 					campoUnoSeleccionado = false;
 					campoDosSeleccionado = false;
 					this->datos.puerto = campoUno;
@@ -193,7 +211,7 @@ datosConexion Vista::cargarPantallaIngresoDatos(bool aviso, int numeroPantalla){
 				}
 				break;
 			case 3:
-				if(textura[15]->aplicarPosicionDeBoton(338,475,&e)){
+				if(texturaBotonConectar->aplicarPosicionDeBoton(338,475,&e)){
 					campoUnoSeleccionado = false;
 					campoDosSeleccionado = false;
 					this->datos.nombre = campoUno;
@@ -206,18 +224,18 @@ datosConexion Vista::cargarPantallaIngresoDatos(bool aviso, int numeroPantalla){
 		if((aviso)&&((campoUno != " ")||(campoDos != " "))){
 			switch(numeroPantalla){
 				case 2:
-					textura[14]->aplicarPosicion(205,530,0,SDL_FLIP_NONE);
+					textoDatosNoCoinciden->aplicarPosicion(205,530,0,SDL_FLIP_NONE);
 					break;
 				case 3:
-					textura[14]->aplicarPosicion(215,530,0,SDL_FLIP_NONE);
+					textoDatosNoCoinciden->aplicarPosicion(215,530,0,SDL_FLIP_NONE);
 					break;
 			}
 		}
 
-		textura[11]->aplicarPosicion(254,560,0,SDL_FLIP_NONE);
-		textura[11]->setAlpha(200);
-		textura[3]->aplicarPosicionConTamanio(0,0,800,600);
-		textura[3]->setAlpha(this->opacidad);
+		textoTPTaller->aplicarPosicion(254,560,0,SDL_FLIP_NONE);
+		textoTPTaller->setAlpha(200);
+		texturaEfectoLuz->aplicarPosicionConTamanio(0,0,800,600);
+		texturaEfectoLuz->setAlpha(this->opacidad);
 		ventana->actualizar();
 
 		this->controlador->botonCerrarVentanaSeleccionado();
@@ -236,21 +254,21 @@ void Vista::transicionDePantalla(){
 	if (!this->controlador->comprobarCierreVentana()){
 		for (this->opacidad = 0; this->opacidad <255; this->opacidad = this->opacidad+5){
 			ventana->limpiar();
-			textura[0]->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
-			textura[1]->aplicarPosicion(180,40,0,SDL_FLIP_NONE);
-			textura[9]->aplicarPosicion(312,337,0,SDL_FLIP_NONE);
-			textura[10]->aplicarPosicion(312,402,0,SDL_FLIP_NONE);
-			textura[7]->aplicarPosicionDeFrame(310-x,358-y,0,-angulo,SDL_FLIP_NONE);
-			textura[8]->aplicarPosicionDeFrame(310+x,423-y,0,angulo,SDL_FLIP_NONE);
-			textura[12]->aplicarPosicion(380-x,368-y,-angulo,SDL_FLIP_NONE);
-			textura[13]->aplicarPosicion(360+x,433-y,angulo,SDL_FLIP_NONE);
-			textura[4]->aplicarPosicionDeFrame(656,550,0,0,SDL_FLIP_NONE);
-			textura[5]->aplicarPosicionDeFrame(30,550,0,0,SDL_FLIP_NONE);
-			textura[6]->aplicarPosicionDeFrame(338,475,0,0,SDL_FLIP_NONE);
-			textura[11]->aplicarPosicion(254,560,0,SDL_FLIP_NONE);
-			textura[11]->setAlpha(200);
-			textura[3]->aplicarPosicionConTamanio(0,0,800,600);
-			textura[3]->setAlpha(this->opacidad);
+			texturaMenuFondo->aplicarPosicion(0,0,0,SDL_FLIP_NONE);
+			texturaMenuMetalSlug->aplicarPosicion(180,40,0,SDL_FLIP_NONE);
+			textoIngresePuerto->aplicarPosicion(312,337,0,SDL_FLIP_NONE);
+			textoIngreseIP->aplicarPosicion(312,402,0,SDL_FLIP_NONE);
+			campoTextoUno->aplicarPosicionDeFrame(310-x,358-y,0,-angulo,SDL_FLIP_NONE);
+			campoTextoDos->aplicarPosicionDeFrame(310+x,423-y,0,angulo,SDL_FLIP_NONE);
+			entradaTextoPuerto->aplicarPosicion(380-x,368-y,-angulo,SDL_FLIP_NONE);
+			entradaTextoIP->aplicarPosicion(360+x,433-y,angulo,SDL_FLIP_NONE);
+			texturaBotonSalir->aplicarPosicionDeFrame(656,550,0,0,SDL_FLIP_NONE);
+			texturaBotonCreditos->aplicarPosicionDeFrame(30,550,0,0,SDL_FLIP_NONE);
+			texturaBotonIngresar->aplicarPosicionDeFrame(338,475,0,0,SDL_FLIP_NONE);
+			textoTPTaller->aplicarPosicion(254,560,0,SDL_FLIP_NONE);
+			textoTPTaller->setAlpha(200);
+			texturaEfectoLuz->aplicarPosicionConTamanio(0,0,800,600);
+			texturaEfectoLuz->setAlpha(this->opacidad);
 			angulo = angulo + 5;
 			x = x+5;
 			y = y+2;
