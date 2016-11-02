5af0d69705d9c0a731efa0d2e92bf10e02fef2c4
ChrisFlorez <chris.florez.d.c@gmail.com>
2016-11-02 14:34:42 -0300

    ahora si funciona lo del zindex jugador

--- TP1/MainCliente.cpp
+++ TP1/MainCliente.cpp
@@ -41,8 +41,7 @@ int stringToInt(string atributo) {
 
 bool chequearSocket(string ip, int puerto) {
 	//string ipServer = "192.168.1.11";
-	string ipServer = "10.1.77.13";
-	//string ipServer = "127.0.0.1";
+	string ipServer = "127.0.0.1";
 	int puertoDeEscucha = 7891;
 
 	return (ip == ipServer && puerto == puertoDeEscucha);
