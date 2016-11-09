832b021293fa950434783e052cf417d512e7e017
ChrisFlorez <chris.florez.d.c@gmail.com>
2016-11-09 17:10:15 -0300

    funciona todoooo

---
+++ TP1/ParametrosMovimiento.cpp
@@ -0,0 +1,20 @@
+/*
+ * ParametrosMovimiento.cpp
+ *
+ *  Created on: 9 nov. 2016
+ *      Author: chris
+ */
+
+#include "ParametrosMovimiento.h"
+
+ParametrosMovimiento::ParametrosMovimiento(Servidor* servidorActual, Jugador* jugadorActual) {
+	this->servidor = servidorActual;
+	this->jugador = jugadorActual;
+	// TODO Auto-generated constructor stub
+
+}
+
+ParametrosMovimiento::~ParametrosMovimiento() {
+	// TODO Auto-generated destructor stub
+}
+
