2c82406f86f5493d7842c89a788dd5c967e00d6a
Franco Mazzoni <franco.mazzoni73@gmail.com>
2016-11-29 19:54:05 -0300

    Se empieza con la implementacion del segundo boss

--- TP1/TaniOh.h
+++ TP1/TaniOh.h
@@ -8,12 +8,13 @@
 #ifndef TANIOH_H_
 #define TANIOH_H_
 #include "Boss.h"
+#define VELOCIDADTANIOH 3
 
 class TaniOh: public Boss {
 public:
 	TaniOh(string nombre, string x, string y);
 	virtual ~TaniOh();
-	virtual void comportamiento(SDL_Rect camara);
+	virtual void comportamiento(SDL_Rect camara, bool tieneQueDisparar);
 };
 
 #endif /* TANIOH_H_ */
