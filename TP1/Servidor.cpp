61cc3e641a70f25a0e0b9f09a00f34cd763a985d
josefinamont <mariajosefina.mont@gmail.com>
2016-12-07 18:04:22 -0300

    Se modifica velocida del proyectil y del jugador

--- TP1/Servidor.cpp
+++ TP1/Servidor.cpp
@@ -22,8 +22,8 @@ Servidor::Servidor(char* nombreArchivoDeUsuarios, int puerto, Logger* logger) {
 	/* Set port number, using htons function to use proper byte order */
 	this->serverAddr.sin_port = htons(puerto);
 	/* Set IP address to localhost */
-	//this->serverAddr.sin_addr.s_addr = inet_addr("192.168.1.11");
-	this->serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	this->serverAddr.sin_addr.s_addr = inet_addr("192.168.1.10");
+	//this->serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	//this->serverAddr.sin_addr.s_addr = inet_addr("10.1.77.13");
 	/* Set all bits of the padding field to 0 */
@@ -764,32 +764,33 @@ void Servidor::calcularPuntajes()
 	usleep(5000000); //tiempo para mostrar la pantalla de puntajes antes de pasar al proximo nivel
 }
 
-void Servidor::avanzarDeNivel()
-{
-	this->nivelActual++;
-	if (this->nivelActual >= CANTIDADNIVELES)
-	{
-		this->gameComplete = true;
-	}
-	if (!this->gameComplete)
-	{
-		this->resetearPosiciones();
-		for (int i = 0; i < jugadores->size(); i++){
-			jugadores->at(i)->setearPlataformas(this->getNivelActual()->plataformas);
+void Servidor::avanzarDeNivel() {
+	//if (contadorJugadores%this->jugadores->size() == 0) {
+		this->nivelActual++;
+		if (this->nivelActual >= CANTIDADNIVELES)
+		{
+			this->gameComplete = true;
 		}
-		string mensajeAvanzarNivel = "9|0|0|" + serializarCapas() + "#";
-		Mensaje* mensajeNivel = new Mensaje("Servidor","Todos",mensajeAvanzarNivel);
-		encolarMensajeProcesadoParaCadaCliente(*mensajeNivel,mensajeAvanzarNivel);
-		mensajeNivel->~Mensaje();
-	}
-	else
-	{
-		string stringJuegoTerminado = "10|#";
-		Mensaje* mensajeJuegoTerminado = new Mensaje("Servidor","Todos",stringJuegoTerminado);
-		encolarMensajeProcesadoParaCadaCliente(*mensajeJuegoTerminado,stringJuegoTerminado);
-		mensajeJuegoTerminado->~Mensaje();
-		usleep (5000000);
-	}
+		if (!this->gameComplete)
+		{
+			this->resetearPosiciones();
+			for (int i = 0; i < jugadores->size(); i++){
+				jugadores->at(i)->setearPlataformas(this->getNivelActual()->plataformas);
+			}
+			string mensajeAvanzarNivel = "9|0|0|" + serializarCapas() + "#";
+			Mensaje* mensajeNivel = new Mensaje("Servidor","Todos",mensajeAvanzarNivel);
+			encolarMensajeProcesadoParaCadaCliente(*mensajeNivel,mensajeAvanzarNivel);
+			mensajeNivel->~Mensaje();
+		}
+		else
+		{
+			string stringJuegoTerminado = "10|#";
+			Mensaje* mensajeJuegoTerminado = new Mensaje("Servidor","Todos",stringJuegoTerminado);
+			encolarMensajeProcesadoParaCadaCliente(*mensajeJuegoTerminado,stringJuegoTerminado);
+			mensajeJuegoTerminado->~Mensaje();
+			usleep(5000000);
+		}
+	//}
 }
 
 bool Servidor::verificarColisionConJugadores(Proyectil* proyectil) {
